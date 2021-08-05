2e1e98a2d5a83b2cdf41405ca0c79cc46d60aa2c
KacperKluska <kacperkluska130@gmail.com>
2021-08-05 18:55:39 +0200

    Second navbar added

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import "./index.scss";
+
+export default function Navbar() {
+  return (
+    <nav id="secondNavbar">
+      <ul id="thirdList">
+        <li className="thirdNavbaritem">
+          <a href="#">Dupa</a>
+        </li>
+        <li className="thirdNavbaritem">
+          <a href="#">Cyce</a>
+        </li>
+        <li className="thirdNavbaritem">
+          <a href="#">i</a>
+        </li>
+        <li className="thirdNavbaritem">
+          <a href="#">Donice</a>
+        </li>
+        <li className="thirdNavbaritem">
+          <a href="#">!</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
