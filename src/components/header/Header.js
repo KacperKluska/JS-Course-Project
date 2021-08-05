3cd733f7e3ffcae94bdb58cda943000a2f6748d2
KacperKluska <kacperkluska130@gmail.com>
2021-08-05 18:55:14 +0200

    Header css changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,33 @@
-import NavButton from '../navButton/NavButton'
-import './index.scss'
+import NavButton from "../navButton/NavButton";
+import "./index.scss";
 
-export default function Header(){
-    return (
-        <header>
-            <nav>
-                <ul id="leftNav">
-                    <li><NavButton text="Sign in"/></li>
-                    <li><NavButton text="Register"/></li>
-                    <li><NavButton text="Search"/></li>
-                </ul>
-                <h1>DressUp</h1>
-                <ul id="rightNav">
-                    <li><NavButton text="Gift Certificate"/></li>
-                    <li><NavButton text="Cart"/></li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <nav id="firstNavbar">
+        <ul id="leftNav">
+          <li>
+            <NavButton text="Sign in" />
+          </li>
+          <li>
+            <NavButton text="Register" />
+          </li>
+          <li>
+            <NavButton text="Search" />
+          </li>
+        </ul>
+        <h1>
+          <a href="#">DressUp</a>
+        </h1>
+        <ul id="rightNav">
+          <li>
+            <NavButton text="Gift Certificate" />
+          </li>
+          <li>
+            <NavButton text="Cart" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
