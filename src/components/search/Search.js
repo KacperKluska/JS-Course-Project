f2c9b83e618c13eeecb9109b98bf34279db5d609
KacperKluska <kacperkluska130@gmail.com>
2021-08-06 16:30:27 +0200

    search div added

---
+++ src/components/search/Search.js
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export default function Search() {
+  return (
+    <div id="search">
+      <input type="text" id="searchInput" placeholder="Search..." />
+    </div>
+  );
+}
