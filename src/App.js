1fa4a5746308dd481daee1e5e12c28325d8e5aeb
KacperKluska <kacperkluska130@gmail.com>
2021-08-06 17:43:07 +0200

    added new photos to main page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.scss";
 import "./fontello/css/fontello.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import zdjecie from "./images/barney5.jpg";
 import Search from "./components/search/Search";
 import OutsideAlerter from "./components/OutsideClick/OutsideAlerter";
 import InfoText from "./components/InfoText/InfoText";
+import MainPhoto from "./images/barney5.jpg";
+import Photo1 from "./images/barney4.jpg";
+import Suit1 from "./images/suit1.jpg";
+import Suit2 from "./images/suit2.jpg";
+import Suit3 from "./images/suit3.jpg";
 
 function App() {
   const handleSearchClose = () => {
@@ -24,7 +28,7 @@ function App() {
       <main>
         <div id="test">
           {/* <img src="./images/barney2.png"></img> */}
-          <img src={zdjecie}></img>
+          <img src={MainPhoto}></img>
         </div>
         <InfoText
           title={"About"}
@@ -32,6 +36,14 @@ function App() {
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis facere saepe veniam iusto a deleniti quam nesciunt dolorum nihil deserunt, tempore consequatur inventore possimus eum neque quo! Consectetur facere quis quibusdam eum possimus provident sequi similique vitae. Perferendis autem aut hic vel officia ut. Vitae neque exercitationem asperiores distinctio laboriosam, officiis illo tempora optio omnis unde nobis perspiciatis rerum reprehenderit nostrum quod magni repellat porro qui numquam, praesentium, necessitatibus perferendis debitis recusandae. Facilis quas, reiciendis tenetur iste repellendus quia reprehenderit numquam nisi at nesciunt sequi! Minus, dicta officiis similique perspiciatis unde sapiente sed commodi quis voluptas veritatis tempore qui temporibus."
           }
         />
+        <div id="test">
+          <img src={Photo1}></img>
+        </div>
+        <div id="flexPhotos">
+          <img src={Suit1}></img>
+          <img src={Suit2}></img>
+          <img src={Suit3}></img>
+        </div>
         <InfoText
           title={"Contact"}
           text={
