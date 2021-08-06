b4569e7ad360d56790c480ff96274609ef409404
KacperKluska <kacperkluska130@gmail.com>
2021-08-06 14:45:30 +0200

    added main photo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.scss";
 import "./fontello/css/fontello.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import zdjecie from "./images/barney5.jpg";
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
-      <div id="test"></div>
+      <div id="test">
+        {/* <img src="./images/barney2.png"></img> */}
+        <img src={zdjecie}></img>
+      </div>
       <Footer />
     </div>
   );
