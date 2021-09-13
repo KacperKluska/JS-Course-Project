6054787b5209f27f03ba96601fab09309e37a2ba
KacperKluska <kacperkluska130@gmail.com>
2021-09-13 22:07:51 +0200

    some visual changes

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -13,40 +13,81 @@ export default function MainPage() {
         <img src={MainPhoto} alt="Barney Stinson"></img>
       </div>
       <InfoText title="About">
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis facere
-        saepe veniam iusto a deleniti quam nesciunt dolorum nihil deserunt,
-        tempore consequatur inventore possimus eum neque quo! Consectetur facere
-        quis quibusdam eum possimus provident sequi similique vitae. Perferendis
-        autem aut hic vel officia ut. Vitae neque exercitationem asperiores
-        distinctio laboriosam, officiis illo tempora optio omnis unde nobis
-        perspiciatis rerum reprehenderit nostrum quod magni repellat porro qui
-        numquam, praesentium, necessitatibus perferendis debitis recusandae.
-        Facilis quas, reiciendis tenetur iste repellendus quia reprehenderit
-        numquam nisi at nesciunt sequi! Minus, dicta officiis similique
-        perspiciatis unde sapiente sed commodi quis voluptas veritatis tempore
-        qui temporibus.
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo velit
+          nulla eius ducimus quia suscipit minus consectetur eaque, in iusto
+          sapiente unde voluptate alias libero delectus nesciunt animi minima
+          totam? Obcaecati molestiae officiis vero totam quas similique aliquid
+          deserunt, ipsa odio minima inventore cumque. Eligendi aliquid hic
+          earum veniam dolorum doloribus sequi quidem nisi obcaecati quibusdam
+          iusto quam quae aut quis assumenda, tempora animi illo iste dolorem
+          aliquam? Voluptates atque iusto totam exercitationem et omnis fugit
+          facere enim ipsa quia nisi harum voluptatem, accusantium blanditiis
+          voluptas eum veniam amet numquam. Voluptate amet laudantium soluta.
+          Assumenda, sapiente commodi maxime voluptatibus facere beatae at vero
+          quae culpa provident, ea inventore omnis nemo nesciunt suscipit cum
+          ipsum autem iure eos dolorum temporibus facilis! Fugit maiores aliquam
+          vel explicabo non et praesentium suscipit hic eveniet repudiandae odio
+          corrupti quisquam laboriosam dicta commodi voluptas beatae neque
+          asperiores nam, ipsum autem corporis officia soluta? Dolore,
+          inventore!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque illum
+          maxime quae qui suscipit, labore recusandae! Aperiam, iste quae
+          consectetur, iusto repudiandae, adipisci asperiores odit libero amet
+          sunt inventore minus? Deleniti repudiandae, odit commodi ea dolor iure
+          fuga vitae nostrum accusamus qui doloremque molestias, totam veritatis
+          aperiam nobis placeat soluta!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim,
+          sapiente illo dignissimos soluta suscipit corporis natus nemo
+          repellat, quam inventore sequi! Assumenda aliquam cum cupiditate ipsum
+          blanditiis reprehenderit consectetur. Illo voluptatibus pariatur
+          dolores aliquam explicabo, odio soluta. Blanditiis illo animi numquam
+          non aperiam quas quasi excepturi atque laudantium impedit.
+        </p>
       </InfoText>
       <div id="test">
         <img src={Photo1} alt="Barney Stinson 2"></img>
       </div>
-      <div id="flexPhotos">
-        <div className="photo">
-          <img src={Suit1} alt="Suit 1"></img>
-        </div>
-        <div className="photo">
-          <img src={Suit2} alt="Suit 2"></img>
-        </div>
-        <div className="photo">
-          <img src={Suit3} alt="Suit 3"></img>
+      <div className="imagesDiv">
+        <div id="flexPhotos">
+          <div className="photo">
+            <img src={Suit1} alt="Suit 1"></img>
+          </div>
+          <div className="photo">
+            <img src={Suit2} alt="Suit 2"></img>
+          </div>
+          <div className="photo">
+            <img src={Suit3} alt="Suit 3"></img>
+          </div>
         </div>
       </div>
       <InfoText title="Contact">
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
-        quisquam omnis fugit hic, nobis eum sit natus ipsum voluptas recusandae
-        commodi molestias temporibus, non voluptatem incidunt dignissimos,
-        suscipit assumenda? Ut, assumenda maxime. Temporibus est perferendis
-        nemo qui accusantium eum, excepturi facilis consectetur totam ex fuga
-        libero in incidunt architecto nam.
+        <p>
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
+          quisquam omnis fugit hic, nobis eum sit natus ipsum voluptas
+          recusandae commodi molestias temporibus, non voluptatem incidunt
+          dignissimos, suscipit assumenda? Ut, assumenda maxime. Temporibus est
+          perferendis nemo qui accusantium eum, excepturi facilis consectetur
+          totam ex fuga libero in incidunt architecto nam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque autem
+          laborum nobis laudantium vitae voluptatem asperiores omnis quam
+          mollitia dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
+          accusamus sapiente eaque hic, laudantium consequuntur consequatur ipsa
+          deserunt reprehenderit asperiores perferendis voluptatibus, doloribus
+          animi cumque optio iure veniam, eveniet culpa aspernatur. Amet,
+          nostrum similique ratione, atque, est adipisci aliquam enim sapiente
+          quasi iure provident ea deserunt officia perspiciatis doloremque
+          recusandae.
+        </p>
       </InfoText>
     </div>
   );
