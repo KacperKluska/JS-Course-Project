6cd93e84da8713bc29196d04e9555ff90826038c
KacperKluska <kacperkluska130@gmail.com>
2021-09-14 13:49:34 +0200

    added Footer tests

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -2,11 +2,16 @@ import { shallow, render } from "enzyme";
 import renderer from "react-test-renderer";
 import Footer from "./Footer";
 
-test("footer text test", () => {
+test("footer text", () => {
   const wrapper = shallow(<Footer />);
   expect(wrapper.find("footer").text()).toContain(
     `Copyrigth © Kacper Kluska 2021. JavaScript course`
   );
+});
+
+test("footer text", () => {
+  const wrapper = shallow(<Footer />);
+  expect(wrapper.type()).toBe("footer");
 });
 
 test("footer text snapshot", () => {
