57255301bdd059ca88dee170dcea2b6f746265ac
KacperKluska <kacperkluska130@gmail.com>
2021-09-14 13:59:08 +0200

    empty CustomLink test

---
+++ src/components/CustomLink/CustomLink.test.js
@@ -0,0 +1,18 @@
+import { shallow, render } from "enzyme";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import renderer from "react-test-renderer";
+import CustomLink from "./CustomLink";
+
+// test("Link test 1", () => {
+//   const wrapper = render(
+//     <Router>
+//       <CustomLink path="/mainPage">Test link</CustomLink>
+//     </Router>
+//   );
+//   expect(wrapper).toContainElement(<Link />);
+// });
+/**
+ * TODO
+ * make more tests
+ */
+test("", () => {});
