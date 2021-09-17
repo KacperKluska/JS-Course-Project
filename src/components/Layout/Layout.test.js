6cb521c7f7128bb9a1117b77df6ac14b09c70138
KacperKluska <kacperkluska130@gmail.com>
2021-09-17 20:47:53 +0200

    added basic Layout tests

---
+++ src/components/Layout/Layout.test.js
@@ -0,0 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Navbar from "../Navbar/Navbar";
+import Search from "../Search/Search";
+import Layout from "./Layout";
+
+let wrapper;
+
+beforeAll(() => {
+  wrapper = mount(
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
+});
+
+test("renders", () => {
+  expect(wrapper).not.toBe(null);
+  expect(wrapper.find(Header)).not.toBe(null);
+  expect(wrapper.find(Navbar)).not.toBe(undefined);
+  expect(wrapper.find(Search)).not.toBe(undefined);
+  expect(wrapper.find(Footer)).not.toBe(undefined);
+});
