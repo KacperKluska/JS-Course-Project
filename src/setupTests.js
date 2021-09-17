e90674ddd8e7e6bcd088b6ce0d456176444cc82d
KacperKluska <kacperkluska130@gmail.com>
2021-09-17 20:39:21 +0200

    added enzyme-to-json and changed setupTests.js to have globals

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,21 @@
-import "@testing-library/jest-dom/extend-expect";
-import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import "@testing-library/jest-dom";
+import React from "react";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import { createSerializer, toJson } from "enzyme-to-json";
+// import sinon from "sinon";
 
-configure({ adapter: new Adapter() });
+//Set the default serializer for Jest to be the from enzyme-to-json
+//This produces an easier to read (for humans) serialized format.
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+//React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+//Make Enzyme functions available in all test files without importing
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.toJson = toJson;
+// global.sinon = sinon;
