a034f1ea0f255e94f1f2f92bcdac1bc1861b681c
KacperKluska <kacperkluska130@gmail.com>
2021-09-21 14:51:27 +0200

    changed footer and added newsletter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,24 @@
+import Newsletter from "../Newsletter/Newsletter";
 import "./style.scss";
 
 export default function Footer() {
   return (
-    <footer>Copyrigth &copy; Kacper Kluska 2021. JavaScript course</footer>
+    <footer className="footer">
+      <div className="footerCollumns">
+        <div className="footerCollumn">
+          <b>Help</b>
+          <label>Returns</label>
+          <label>Change</label>
+          <label>FAQ</label>
+        </div>
+        <div className="footerCollumn">
+          <b>Payment and shipment</b>
+          <label>Payment forms</label>
+          <label>Shipment forms</label>
+        </div>
+        <Newsletter />
+      </div>
+      <b>Copyrigth &copy; Kacper Kluska 2021. JavaScript course</b>
+    </footer>
   );
 }
