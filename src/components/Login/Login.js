448fd7adf95d9038314381ea1ccbe0a7418a416a
KacperKluska <kacperkluska130@gmail.com>
2021-10-02 23:30:39 +0200

    Login page changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,46 @@
 import "./style.scss";
 import CustomLink from "../CustomLink/CustomLink";
+import Sectionlogo from "../SectionLogo/SectionLogo";
 
 export default function Login() {
   return (
     <div className="emptyScreen">
-      <form action="">
-        <section id="login">
+      <Sectionlogo>Sign in!</Sectionlogo>
+      <section className="login">
+        <form className="loginForm">
           <header>
-            <h1>Login</h1>
+            <h1>Sign in</h1>
           </header>
           <div className="inputs">
-            <input type="email" placeholder="Email"></input>
-            <input type="password" placeholder="Password"></input>
+            Email
+            <input type="email" placeholder="Email..."></input>
+            Password
+            <input type="password" placeholder="Password..."></input>
           </div>
           <div className="submitButtons">
             <CustomLink path="/error">
-              <div id="underlineLabel">Forgot password ?</div>
+              <div className="underlineLink">Forgot password ?</div>
             </CustomLink>
             <button className="submitButton" type="submit">
               Login
             </button>
           </div>
-        </section>
-      </form>
+        </form>
+        <div className="loginInfo">
+          <h1>
+            <b>Register now for more!</b>
+          </h1>
+          <ul className="loginInfoList">
+            <li>Watch your package status</li>
+            <li>Shopping history</li>
+            <li>Posibility of getting discount</li>
+            <li>Autocomplete form</li>
+          </ul>
+          <CustomLink path="/register">
+            <button className="loginInfoButton">Sign up</button>
+          </CustomLink>
+        </div>
+      </section>
     </div>
   );
 }
