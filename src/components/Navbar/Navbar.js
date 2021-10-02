0d6da60b409827f7d3ee5d1713ceb016c9afdb25
KacperKluska <kacperkluska130@gmail.com>
2021-10-03 01:05:33 +0200

    navbar About and Contact works now

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import NavItem from "../NavItem/NavItem";
+import CustomLink from "../CustomLink/CustomLink";
 import "./style.scss";
 
 export default function Navbar() {
   const scrollToElById = (id) => {
-    const offset = document.getElementById(`${id}`).offsetTop;
-    window.scrollTo({ top: offset - 50, behavior: "smooth" });
+    setTimeout(() => {
+      const element = document.getElementById(`${id}`);
+      if (element === null) {
+        return;
+      }
+      const offset = element.offsetTop;
+      window.scrollTo({ top: offset - 50, behavior: "smooth" });
+    }, 0);
   };
 
   /**
@@ -19,8 +27,17 @@ export default function Navbar() {
           <NavItem>For him</NavItem>
           <NavItem>For her</NavItem>
           <NavItem>Kids</NavItem>
-          <NavItem onClick={() => scrollToElById("About")}>About</NavItem>
-          <NavItem onClick={() => scrollToElById("Contact")}>Contact</NavItem>
+          {/* <CustomLink to="/">Elo</CustomLink> */}
+          <NavItem onClick={() => scrollToElById("About")}>
+            <Link className="navbarLink" to="/">
+              About
+            </Link>
+          </NavItem>
+          <NavItem onClick={() => scrollToElById("Contact")}>
+            <Link className="navbarLink" to="/">
+              Contact
+            </Link>
+          </NavItem>
         </ul>
       </div>
     </nav>
