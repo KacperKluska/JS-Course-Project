a3d8fcc42fbb4bf5668f5c62877116f4e8398a22
KacperKluska <kacperkluska130@gmail.com>
2021-10-26 23:49:25 +0200

    added messege, work with backend, className change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,67 @@
 import './style.scss';
+import { useState } from 'react/cjs/react.development';
 import CustomLink from '../CustomLink/CustomLink';
 import Sectionlogo from '../SectionLogo/SectionLogo';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    try {
+      const singInResponse = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const signUpData = await singInResponse.json();
+      if (singInResponse.status !== 200) {
+        setError(true);
+        setMessage(signUpData.error);
+      } else {
+        setError(false);
+        setMessage(signUpData.message);
+      }
+    } catch (err) {
+      console.log('🚀 ~ file: Register.js ~ line 33 ~ registerUser ~ err', err);
+    }
+  };
+
   return (
     <div className="emptyScreen">
       <Sectionlogo>Sign in!</Sectionlogo>
       <section className="login">
-        <form className="loginForm">
+        <form className="loginForm" onSubmit={signIn}>
           <header>
             <h1>Sign in</h1>
           </header>
-          <div className="inputs">
+          <div className="loginInputs">
             Email
-            <input type="email" placeholder="Email..." required />
+            <input
+              className={`${error && 'failure'}`}
+              type="email"
+              placeholder="Email..."
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             Password
-            <input type="password" placeholder="Password..." required />
+            <input
+              className={`${error && 'failure'}`}
+              type="password"
+              placeholder="Password..."
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="submitButtons">
             <CustomLink path="/error">
@@ -41,6 +87,9 @@ export default function Login() {
               Sign up
             </button>
           </CustomLink>
+          <h3 className={`loginInfoMessage ${!error ? 'success' : 'failure'}`}>
+            {message}
+          </h3>
         </div>
       </section>
     </div>
