d8d43a75829bd15beb9729967010675b8c43262c
KacperKluska <kacperkluska130@gmail.com>
2021-10-30 17:41:36 +0200

    added id state to Context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,6 +2,9 @@ import { React } from 'react';
 
 export const UserContext = React.createContext('');
 
-export const UserContextProvider = ({ children }) => (
-  <UserContext.Provider>{children}</UserContext.Provider>
-);
+export const UserContextProvider = ({ children }) => {
+  const [id, setId] = React.useState('');
+  return (
+    <UserContext.Provider value={[id, setId]}>{children}</UserContext.Provider>
+  );
+};
