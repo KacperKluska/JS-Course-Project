64e20a33d562d7ac82918455d3fb1ad60b080c5c
KacperKluska <kacperkluska130@gmail.com>
2021-11-22 00:32:27 +0100

    moved logic to logic.js and added shipment details

--- src/components/Pages/AccountPage/AccountPage.js
+++ src/components/Pages/AccountPage/AccountPage.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import './style.scss';
 import SectionLogo from '../../SectionLogo/SectionLogo';
-import { getUserData, refreshToken } from '../../../services/requests';
+import { refreshToken } from '../../../services/requests';
+import {
+  defaultUserData,
+  defaultUserShipmentData,
+  getData,
+  getShipmentData,
+  getUserInputsData,
+  getUserShipmentInputsData,
+} from './logic';
 
 const AccountPage = () => {
-  const [name, setName] = React.useState('');
-  const [surname, setSurname] = React.useState('');
-  const [email, setEmail] = React.useState('');
+  const [userData, setUserData] = React.useState({
+    ...defaultUserData,
+  });
 
-  const getData = async () => {
-    try {
-      const { data, status } = await getUserData();
-      if (status === 200) {
-        setName(data.name);
-        setSurname(data.surname);
-        setEmail(data.email);
-      } else {
-        setName('Server error');
-        setSurname('Server error');
-        setEmail('Server error');
-      }
-    } catch (err) {
-      setName('Server error');
-      setSurname('Server error');
-      setEmail('Server error');
-    }
-  };
+  const [userShipmentData, setUserShipmentData] = React.useState({
+    ...defaultUserShipmentData,
+  });
 
-  React.useEffect(() => {
-    refreshToken();
-    getData();
+  const userInputsData = getUserInputsData(userData);
+  const userShipmentInputsData = getUserShipmentInputsData(userShipmentData);
+
+  React.useEffect(async () => {
+    await refreshToken();
+    await getData(setUserData);
+    await getShipmentData(setUserShipmentData);
   }, []);
 
   return (
@@ -40,43 +37,23 @@ const AccountPage = () => {
           <header className="sectionTitle">
             <h2>Account settings</h2>
           </header>
-          <label htmlFor="name" className="accountInput">
-            Name:
-            <input type="text" id="name" value={name} readOnly />
-          </label>
-          <label htmlFor="surname" className="accountInput">
-            Surname:
-            <input type="text" id="surname" value={surname} readOnly />
-          </label>
-          <label htmlFor="email" className="accountInput">
-            Email:
-            <input type="text" id="email" value={email} readOnly />
-          </label>
+          {userInputsData.map((item) => (
+            <label htmlFor={item.id} className="accountInput">
+              {item.label}:
+              <input type="text" id={item.id} value={item.value} readOnly />
+            </label>
+          ))}
         </section>
         <section className="shipmentData">
           <header className="sectionTitle">
             <h2>Shipment details</h2>
           </header>
-          <label htmlFor="country" className="accountInput">
-            Country:
-            <input type="text" id="country" value={name} readOnly />
-          </label>
-          <label htmlFor="city" className="accountInput">
-            City:
-            <input type="text" id="city" value={surname} readOnly />
-          </label>
-          <label htmlFor="street" className="accountInput">
-            Street:
-            <input type="text" id="street" value={email} readOnly />
-          </label>
-          <label htmlFor="house_number" className="accountInput">
-            House number:
-            <input type="text" id="house_number" value={surname} readOnly />
-          </label>
-          <label htmlFor="phone_number" className="accountInput">
-            Phone number:
-            <input type="text" id="phone_number" value={email} readOnly />
-          </label>
+          {userShipmentInputsData.map((item) => (
+            <label htmlFor={item.id} className="accountInput">
+              {item.label}:
+              <input type="text" id={item.id} value={item.value} readOnly />
+            </label>
+          ))}
         </section>
       </div>
     </article>
