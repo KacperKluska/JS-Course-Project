09ab2aa64112787b0dd103fb00363f274ccaaba0
KacperKluska <kacperkluska130@gmail.com>
2021-11-28 15:09:34 +0100

    Added cards template

--- src/components/Pages/ProductsPage/ProductsPage.js
+++ src/components/Pages/ProductsPage/ProductsPage.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import './style.scss';
+import React, { useState, useEffect } from 'react';
+import { getAllProducts } from '../../../services/requests';
+import ProductFilters from '../../ProductFilters/ProductFilters';
 
 function ProductsPage() {
-  return <div className="emptyScreen">here is your products</div>;
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    setLoading(true);
+    setProducts(await getAllProducts());
+    setLoading(false);
+  }, []);
+
+  return (
+    <article className="emptyScreen">
+      <ProductFilters />
+      <section className="products">
+        {loading && <div>Loading...</div>}
+        {!loading && (
+          <header className="productsHeader">
+            {products.data[0].category}
+          </header>
+        )}
+        {!loading && (
+          <section className="productsList">
+            {products.data.map((product) => (
+              <div className="productCard" key={product.name}>
+                <div className="productCardImage">img here</div>
+                <div className="productCardDescription">
+                  <h3>{product.name}</h3>
+                  {product.price} $
+                </div>
+                <div className="productCardButton">
+                  <button type="button">Show more</button>
+                </div>
+              </div>
+            ))}
+          </section>
+        )}
+      </section>
+    </article>
+  );
 }
 
 export default ProductsPage;
