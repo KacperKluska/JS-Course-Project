c8ac76b14581796a40629504ac48b77b59ef9564
KacperKluska <kacperkluska130@gmail.com>
2021-11-29 01:07:14 +0100

    animated selects and fetch filters

--- src/components/ProductFilters/ProductFilters.js
+++ src/components/ProductFilters/ProductFilters.js
@@ -1,28 +1,32 @@
+/* eslint-disable prefer-destructuring */
 import './style.scss';
 import React, { useState, useEffect } from 'react';
+import { getAllProductsFilters } from '../../services/requests';
 
 function ProductFilters() {
+  const [selectedFilter, setSelectedFilter] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [filtersArrays, setFiltersArrays] = useState([]);
+
   const filters = [
-    { name: 'Category', open: false },
-    { name: 'Pattern', open: false },
-    { name: 'Figure', open: false },
-    { name: 'Color', open: false },
-    { name: 'Type', open: false },
+    { name: 'Pattern' },
+    { name: 'Color' },
+    { name: 'Figure' },
+    { name: 'Type' },
+    { name: 'Category' },
   ];
 
-  /**
-   * TODO
-   * change open in object to lists of filters
-   * (like, for color: [brown, black, white])
-   * and add then map in filtersSelectList
-   */
+  // , setChoseValues
 
-  const [selectedFilter, setSelectedFilter] = useState({});
+  const [choseValues] = useState([
+    { name: null },
+    { name: null },
+    { name: null },
+    { name: null },
+    { name: null },
+  ]);
 
   const handleListShow = (newFilter) => {
-    console.log(newFilter);
-    console.log(selectedFilter);
-    console.log(newFilter.name === selectedFilter.name);
     if (
       Object.keys(selectedFilter).length === 0 ||
       newFilter.name !== selectedFilter.name
@@ -31,32 +35,59 @@ function ProductFilters() {
     else setSelectedFilter({});
   };
 
-  useEffect(() => {}, [filters]);
+  useEffect(async () => {
+    const response = await getAllProductsFilters();
+    setFiltersArrays(response.data);
+    setLoading(false);
+  }, []);
 
   return (
     <section className="productFilters">
-      {filters.map((filter) => (
-        <div className="filter" key={filter.name}>
-          {filter.name}
-          <div className="filtersSelect">
-            <button
-              type="button"
-              onClick={() => handleListShow(filter)}
-              className="filtersSelectButton"
-            >
-              <span>{filter.name} (select)</span>
-              <i className="icon-down-open" />
-            </button>
-            {selectedFilter.name === filter.name && (
-              <ul className="filtersSelectList">
-                <li className="filtersSelectItem">1</li>
-                <li className="filtersSelectItem">2</li>
-                <li className="filtersSelectItem">3</li>
-              </ul>
-            )}
+      {loading && <span>Loading...</span>}
+      {!loading &&
+        filters.map((filter, index) => (
+          <div className="filter" key={filter.name}>
+            {filter.name}
+            <div className="filtersSelect">
+              <button
+                type="button"
+                onClick={() => handleListShow(filter)}
+                className="filtersSelectButton"
+              >
+                <span>
+                  {filter.name}:{' '}
+                  {choseValues[index].name === null
+                    ? '(select)'
+                    : choseValues[index].name}
+                </span>
+                <i
+                  className={`${
+                    selectedFilter.name === filter.name
+                      ? 'icon-up-open'
+                      : 'icon-down-open'
+                  }`}
+                />
+              </button>
+              {selectedFilter.name === filter.name && (
+                <ul className="filtersSelectList">
+                  {filtersArrays[index].array.map((item) => (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        console.log(item.value);
+                        handleListShow(filter);
+                      }}
+                      key={item.id}
+                      className="filtersSelectItem"
+                    >
+                      {item.value}
+                    </button>
+                  ))}
+                </ul>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       Price from:
       <input
         className="priceInput"
