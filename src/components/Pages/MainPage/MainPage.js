77fdcf8084216d18f73ade83e8b062bd56144be8
KacperKluska <kacperkluska130@gmail.com>
2021-12-13 23:53:40 +0100

    Restyled main page

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -1,11 +1,7 @@
 import './style.scss';
 import InfoText from '../../InfoText/InfoText';
-import headerPhoto from '../../../images/barney6.jpg';
-import asidePhoto from '../../../images/barney3.jpg';
-import suit1 from '../../../images/suit4.jpg';
-import suit2 from '../../../images/suit2.jpg';
-import suit3 from '../../../images/suit3.jpg';
 import { navItems } from '../../Navbar/Navbar';
+import ShipmentBonuses from '../../ShipmentBonuses/ShipmentBonuses';
 
 export default function MainPage() {
   return (
@@ -16,71 +12,27 @@ export default function MainPage() {
           alt="Barney Stinson"
         />
       </div>
-      <div className="imagesDiv">
-        <h1 className="title">
-          <b>Check out our new collection</b>
-        </h1>
-        <div className="gridCollage">
-          <div className="photo headerImg">
-            <img src={headerPhoto} alt="Suit 1" />
-          </div>
-          <div className="photo asideImg">
-            <img src={asidePhoto} alt="Suit 1" />
-          </div>
-          <div className="photo normalIMG">
-            <img src={suit1} alt="Suit 1" />
-          </div>
-          <div className="photo normalIMG">
-            <img src={suit2} alt="Suit 2" />
-          </div>
-          <div className="photo normalIMG">
-            <img src={suit3} alt="Suit 3" />
-          </div>
-        </div>
-      </div>
-      <InfoText title="Contact" scrollRef={navItems[3].ref}>
+      <ShipmentBonuses />
+      <section className="mainPageImage">
+        <img
+          className="image"
+          src="http://localhost:3001/get/image/main_page_suit_1.jpg"
+          alt="hi"
+        />
+        <div className="mainPageImageText">Winter collection 50% on sale</div>
+        <button className="mainPageImageButton" type="button">
+          Show
+        </button>
+      </section>
+      <InfoText title="About Us" scrollRef={navItems[2].ref}>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
-          quisquam omnis fugit hic, nobis eum sit natus ipsum voluptas
-          recusandae commodi molestias temporibus, non voluptatem incidunt
-          dignissimos, suscipit assumenda? Ut, assumenda maxime. Temporibus est
-          perferendis nemo qui accusantium eum, excepturi facilis consectetur
-          totam ex fuga libero in incidunt architecto nam.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque autem
-          laborum nobis laudantium vitae voluptatem asperiores omnis quam
-          mollitia dolorum?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
-          accusamus sapiente eaque hic, laudantium consequuntur consequatur ipsa
-          deserunt reprehenderit asperiores perferendis voluptatibus, doloribus
-          animi cumque optio iure veniam, eveniet culpa aspernatur. Amet,
-          nostrum similique ratione, atque, est adipisci aliquam enim sapiente
-          quasi iure provident ea deserunt officia perspiciatis doloremque
-          recusandae.
-        </p>
-      </InfoText>
-      <InfoText title="About" scrollRef={navItems[2].ref}>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo velit
-          nulla eius ducimus quia suscipit minus consectetur eaque, in iusto
-          sapiente unde voluptate alias libero delectus nesciunt animi minima
-          totam? Obcaecati molestiae officiis vero totam quas similique aliquid
-          deserunt, ipsa odio minima inventore cumque. Eligendi aliquid hic
-          earum veniam dolorum doloribus sequi quidem nisi obcaecati quibusdam
-          iusto quam quae aut quis assumenda, tempora animi illo iste dolorem
-          aliquam? Voluptates atque iusto totam exercitationem et omnis fugit
-          facere enim ipsa quia nisi harum voluptatem, accusantium blanditiis
-          voluptas eum veniam amet numquam. Voluptate amet laudantium soluta.
-          Assumenda, sapiente commodi maxime voluptatibus facere beatae at vero
-          quae culpa provident, ea inventore omnis nemo nesciunt suscipit cum
-          ipsum autem iure eos dolorum temporibus facilis! Fugit maiores aliquam
-          vel explicabo non et praesentium suscipit hic eveniet repudiandae odio
-          corrupti quisquam laboriosam dicta commodi voluptas beatae neque
-          asperiores nam, ipsum autem corporis officia soluta? Dolore,
-          inventore!
+          <b>SuitUp</b> is an online store with formal clothes for men, women
+          and kids. You can shop the latest trends in fashion suits, sweaters,
+          boots and more.
+          <p>
+            We are online 24/7 at <u>SuitUp.com</u> and you can come and visit
+            our local store and chose you dream suit with a specialist.
+          </p>
         </p>
       </InfoText>
     </div>
